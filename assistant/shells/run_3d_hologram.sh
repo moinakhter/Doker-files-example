c702659ea5a7f19735378a632dacd7463e57ef71
Moin Akhtar <moinakhtar@dizaynvip.com>
2022-09-27 13:39:11 +0300

    deleted unnessary files

--- assistant/shells/run_3d_hologram.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Running Guide:
-# In order to run the assistant face in 3d mode, you need to install chromium using the following command
-# apt-get install chromium-browser
-# now you can open chromium, move the chromium tab to the hologram and run this file.
-
-chromium-browser --incognito --allow-file-access-from-files --window-position=0,0 --window-size=4480,2680 --start-fullscreen --no-default-browser-check --disable-infobars '../assistant/threejs/index.html'
--- assistant/shells/start_main.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-FILE=~/.local/bin/pipenv
-if test -f "$FILE"; then
-    ~/.local/bin/pipenv run python3 -m assistant "$@";
-else
-    python3 -m assistant "$@";
-fi
